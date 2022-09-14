fe0a0dc8285a4634bdb7e351a3124f664e9328c9
Zakaria Kerkeb <z.kerkeb@gmail.com>
2022-09-14 13:44:24 +0100

    feat: test bot in progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log('wesh')
+import dotenv from 'dotenv'
+import { Client, GatewayIntentBits } from 'discord.js'
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return
+
+  if (interaction.commandName === 'pingui') {
+    await interaction.reply('Pong!')
+  }
+})
+
+client.on('messageCreate', async msg => {
+  if (msg.content === 'pingui') {
+    await msg.reply('Pong!')
+    console.log('🚀 ~ file: index.js ~ line 26 ~ msg', msg)
+  }
+})
+
+// const connection = joinVoiceChannel({
+//   channelId: channel.id,
+//   guildId: channel.guild.id,
+//   adapterCreator: channel.guild.voiceAdapterCreator
+// })
+
+client.login(process.env.DISCORD_TOK)
